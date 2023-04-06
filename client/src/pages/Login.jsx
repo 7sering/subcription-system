7ed91ac333894e66e1a220d1b0e6a4c115b59376
coder <codenow8@gmail.com>
2023-04-06 11:39:49 +0545

    reuseble input field and form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,9 +3,16 @@ import React from 'react'
 const Login = () => {
   return (
    <>
-    <div className="container">
-    <h2>Login</h2>
+     <div className='d-flex justify-content-center'>
+  <div className="container align-items-center d-flex">
+    <div className="row col-md-6 offset-md-3 text-center">
+      <h1 className='pt-5 font-bold'>Login your account </h1>
+      <p className='lead pb-5'>
+        Sign Up fo free no credit required
+      </p>
     </div>
+    </div>
+  </div>
    </>
   )
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import { useState } from "react";
+import Input from "../components/Input";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <>
-    <div className="container">
-    <h2>Register</h2>
-    </div>
-   </>
-  )
-}
+      <div className="d-flex justify-content-center">
+        <div className="container align-items-center d-flex">
+          <div className="row col-md-6 offset-md-3 text-center">
+            <h1 className="pt-5 font-bold">Lets Get Started</h1>
+            <p className="lead pb-5">Sign Up fo free no credit required</p>
+            <div className="form-group">
+              <Input label="Name" value={name} setValue={setName} />
+              <Input
+                label="Email"
+                type="email"
+                value={email}
+                setValue={setEmail}
+              />
+              <Input
+                label="Password"
+                type="password"
+                value={password}
+                setValue={setPassword}
+              />
+            </div>
+          </div>
+          {/* <div className="row">
+            <pre>{JSON.stringify({ name, email, password }, null, 4)}</pre>
+          </div> */}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Register
+export default Register;
