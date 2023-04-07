21babc7c990bf3c1038a21d5085b5876a6ce285f
coder <codenow8@gmail.com>
2023-04-07 07:45:23 +0545

    error and validation handling

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,22 @@
-exports.register = (req, res) => {
-  // res.send("API Endpoint Success")
-  res.json({
-    data: "from controller",
-  });
+exports.register = async (req, res) => {
+  try {
+    const { name, password } = req.body;
+    if (!name) {
+      return res.json({
+        error: "Name is required",
+      });
+    }
+    if (!password || password.length < 6) {
+      return res.json({
+        error: "Password is required and min 6 character",
+      });
+    }
+
+    res.json({
+      data: "This is /register endpoint",
+    });
+    console.log(req.body);
+  } catch (error) {
+    console.log(error);
+  }
 };
