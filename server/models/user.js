e23ce5f6a3eac5a55528c684c56fe687d178961d
coder <codenow8@gmail.com>
2023-04-07 11:45:50 +0545

    User Model Added

---
+++ server/models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 8,
+    max: 64,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
