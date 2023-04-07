5d08583eff98d050473154fe718a991efc85c638
coder <codenow8@gmail.com>
2023-04-07 21:33:37 +0545

    User Created Successful

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,12 @@
+const User = require("../models/user");
+const {
+  comparePassword,
+  hasPassword,
+} = require("../helpers/authHelper");
+
 exports.register = async (req, res) => {
   try {
-    const { name, password } = req.body;
+    const { name, email, password } = req.body;
     if (!name) {
       return res.json({
         error: "Name is required",
@@ -12,9 +18,28 @@ exports.register = async (req, res) => {
       });
     }
 
-    res.json({
-      data: "This is /register endpoint",
-    });
+    const exist = await User.findOne({ email });
+    if (exist) {
+      return res.json({
+        error: "Email is already taken",
+      });
+    }
+
+    // HashPassword
+    const hashedPassword = await hasPassword(password);
+    try {
+      const user = await new User({
+        name,
+        email,
+        password: hashedPassword,
+      }).save();
+      console.log(user);
+      const { password, ...rest } = user._doc;
+      return res.json(user)
+    } catch (error) {
+      console.log(error);
+    }
+
     console.log(req.body);
   } catch (error) {
     console.log(error);
