abf4f5e4a1a3975a1de98f46404cd27e292b6f77
coder <codenow8@gmail.com>
2023-04-08 18:18:57 +0545

    Logout Added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,27 +1,49 @@
+import {Fragment} from "react"
 import { Link } from "react-router-dom";
+import { isAuth } from "../utils/functions";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem("auth");
+    navigate("/login");
+  };
   return (
     <>
       <div className="container">
         <div className="d-flex align-items-center justify-content-between ">
-        <h3 className="pt-2 font-weight-bold">OPTHA</h3>
+          <h3 className="pt-2 font-weight-bold">OPTHA</h3>
           <ul className="nav ">
             <li className="nav-item">
               <Link className="nav-link active" to="/">
                 Home
               </Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/register">
-                Register
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/login">
-                Login
-              </Link>
-            </li>
+
+            {isAuth() ? (
+              <Fragment>
+              <li className="nav-item">
+                  <span style={{cursor: "pointer"}} onClick={logout} className="nav-link" >
+                    Logout
+                  </span>
+                </li>
+              </Fragment>
+            ) : (
+              <Fragment>
+              <li className="nav-item">
+                  <Link className="nav-link" to="/register">
+                    Register
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/login">
+                    Login
+                  </Link>
+                </li>
+              </Fragment>
+            )}
           </ul>
         </div>
       </div>
