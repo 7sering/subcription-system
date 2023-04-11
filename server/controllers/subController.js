105c00f035dede5ce767e2cd724a8f56f9b6f6c6
coder <codenow8@gmail.com>
2023-04-11 07:17:03 +0545

    Stripe Checkout Confiuration Complete

--- server/controllers/subController.js
+++ server/controllers/subController.js
@@ -1,11 +1,33 @@
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const User = require("../models/user");
 
-exports.prices = async(req, res) => {
-    const prices = await stripe.prices.list()
-    // console.log("prices", prices);
-    res.json(prices.data.reverse())
-}
+exports.prices = async (req, res) => {
+  const prices = await stripe.prices.list();
+  // console.log("prices", prices);
+  res.json(prices.data.reverse());
+};
 
-exports.createSubscription = async(req, res) => {
-   console.log(req.body);
-}
+exports.createSubscription = async (req, res) => {
+  //    console.log(req.body);
+  try {
+    const user = await User.findById(req.auth._id);
+
+    const session = await stripe.checkout.sessions.create({
+      mode: "subscription",
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price: req.body.priceId,
+          quantity: 1,
+        },
+      ],
+      customer: user.stripe_customer_id,
+      success_url: process.env.STRIPE_SUCCESS_URL,
+      cancel_url: process.env.STRIPE_CANCEL_URL,
+    });
+    console.log("checkout session", session);
+    res.json(session.url);
+  } catch (error) {
+    console.log(error);
+  }
+};
--- server/routes/subs.js
+++ server/routes/subs.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { prices, createSubscription} = require("../controllers/subController");
-const { requireSignin } = require("../middlewares");
-
+const {requireSignin} = require("../middlewares/index")
 
 router.get("/prices", prices);
 router.post("/create-subscription", requireSignin, createSubscription);
